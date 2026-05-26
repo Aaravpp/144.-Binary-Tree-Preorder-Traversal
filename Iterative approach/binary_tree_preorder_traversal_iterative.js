7ab3db4a7dc6e895d17769d497cb0cd452ccb5b0
Arav PP <aravpp03@gmail.com>
2026-05-26 22:47:32 +0530

    binary_tree_preorder_traversal_iterative(Iteratice approach) - js

---
+++ Iterative approach/binary_tree_preorder_traversal_iterative.js
@@ -0,0 +1,14 @@
+var preorderTraversal = function(root) {
+    if(!root) return [];
+
+    let ans = [];
+    let stack = [root];
+
+    while(stack.length){
+        let curr = stack.pop();
+        ans.push(curr.val);
+        curr.right && stack.push(curr.right);
+        curr.left && stack.push(curr.left);
+    }
+    return ans;
+};
